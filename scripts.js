ebedb0034d871c2e40f313f579815272dccbf66a
dsoluk <70490492+dsoluk@users.noreply.github.com>
2022-04-03 21:27:47 -0400

    working on js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function rankSelected (btn,value) {
+    // change color of the button
+    // activate the submit button
+    // get the value of the button
+    //pass the value of the button to the next page
+    // flip thank you card with value 
+
+    // transform: rotateY(180deg);
+    // const rankObj = document.getElementById(btn);
+    document.getElementById(btn).style.backgroundColor =  "#FC7614";
+    document.getElementById(btn).style.color = "#FFFFFF";
+}
