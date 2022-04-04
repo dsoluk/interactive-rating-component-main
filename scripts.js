97153a0c91d6209e8f526fe982fa139d0daf6bac
dsoluk <70490492+dsoluk@users.noreply.github.com>
2022-04-04 19:57:25 -0400

    finished mobile

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
-function rankSelected (btn,value) {
-    // change color of the button
-    // activate the submit button
-    // get the value of the button
-    //pass the value of the button to the next page
-    // flip thank you card with value 
+function rankSelected (btn,val) {
+    // change color and background color of the button
+    let rankObj = document.getElementById(btn.id);
 
-    // transform: rotateY(180deg);
-    // const rankObj = document.getElementById(btn);
-    document.getElementById(btn).style.backgroundColor =  "#FC7614";
-    document.getElementById(btn).style.color = "#FFFFFF";
+    rankObj.style.backgroundColor =  "#FC7614";
+    rankObj.style.color = "#FFFFFF";
+
+    // activate the submit button so can be clicked
+
+    let submitBtn = document.getElementById("submit");
+
+    submitBtn.disabled = false;
+
+    // update result text with selected rank value
+
+    let resultText = document.getElementById("rankResult");
+    
+    resultText.innerHTML = val;
+}
+
+function flipCard() {
+  // flip the card to show thank you and results upon click
+  
+  let flipObj = document.querySelector(".flip-card .flip-card-inner");
+  
+  flipObj.classList.toggle("flipCard");
 }
